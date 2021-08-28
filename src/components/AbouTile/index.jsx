cf2b6d350ae9ee8472fffc6aefa5ba37cf35fd12
gabrielcon6 <gabrielcon6cao@gmail.com>
2021-08-28 14:10:09 +1000

    added space for icones replacing img

--- src/components/AbouTile/index.jsx
+++ src/components/AbouTile/index.jsx
@@ -3,21 +3,14 @@ import style from './about.module.less';
 
 const AboutTile = (props) => {
   const {
-    img, textH4, textH3, alt, height, width,
+    img, textH1, textH4, textH3, alt, height, width,
   } = props;
   return (
     <div className={style.aboutTile}>
-      <div className={style.aboutBlock}>
-        <img
-          src={`../${img}`}
-          height={height || 64}
-          width={width || 64}
-          alt={alt || ''}
-        />
-      </div>
       <div className={`textCenter ${style.mrTp26PX}`}>
-        <h4>{ textH4 || ''}</h4>
-        <h3>{textH3 || '' }</h3>
+        <h1>{textH1 || ''}</h1>
+        <h4>{textH4 || ''}</h4>
+        <h3>{textH3 || ''}</h3>
       </div>
     </div>
   );
