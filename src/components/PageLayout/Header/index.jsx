0931aaa9f73e62bd6def9f11bf128c6a95793c81
gabrielcon6 <gabrielcon6cao@gmail.com>
2021-08-31 10:28:17 +1000

    added resume link to navbar

--- src/components/PageLayout/Header/index.jsx
+++ src/components/PageLayout/Header/index.jsx
@@ -47,7 +47,7 @@ export default () => {
               </Link>
             </li>
             <li className={style.navItem}>
-              <Link to="/resume" onClick={toggleMenu} activeClassName={style.anchorActive}>
+              <Link onClick={toggleMenu} activeClassName={style.anchorActive}>
                 <span ><a style={{ padding: "0px" }} href="https://res.cloudinary.com/gabrielcon6/image/upload/v1594607068/myPortfolio/Gabriel_Resume_13JUL_fxg2rf.pdf" download target="_blank"> Resume &nbsp;  <img src="download.png" height="10px"></img></a></span>
               </Link>
             </li>
