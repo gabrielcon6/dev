06532c9d64ac9bf1f9bcc93d92372c44bd60f4d0
gabrielcon6 <gabrielcon6cao@gmail.com>
2021-09-11 10:02:23 +1000

    changed emojis sizes

--- src/components/AbouTile/index.jsx
+++ src/components/AbouTile/index.jsx
@@ -3,12 +3,12 @@ import style from './about.module.less';
 
 const AboutTile = (props) => {
   const {
-    img, textH1, textH4, textH3, alt, height, width,
+    textH1, textH4, textH3,
   } = props;
   return (
     <div className={style.aboutTile}>
       <div className={`textCenter ${style.mrTp26PX}`}>
-        <h1>{textH1 || ''}</h1>
+        <p className={style.emojis}>{textH1 || ''}</p>
         <h4>{textH4 || ''}</h4>
         <h3>{textH3 || ''}</h3>
       </div>
